411f2215326bc3bc1d2ad2aa2e3a0dbadafcb509
avery <pozxc76@pusan.ac.kr>
2024-01-30 15:06:03 +0900

    springmvc. 기본 기능 - 요청매핑

--- springmvc/src/main/java/hello/springmvc/basic/request/RequestParamController.java
+++ springmvc/src/main/java/hello/springmvc/basic/request/RequestParamController.java
@@ -1,6 +1,5 @@
 package hello.springmvc.basic.request;
 
-import hello.springmvc.basic.HelloData;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
