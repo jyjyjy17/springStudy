18259763a27b4cc997cbc0d49771b1789338b0ae
avery <pozxc76@pusan.ac.kr>
2024-06-11 14:50:01 +0900

    오륨메시지 처리

--- validation-start/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
+++ validation-start/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
@@ -139,23 +139,22 @@ public class ValidationItemControllerV2 {
     }*/
 @PostMapping("/add")
 public String addItemV4(@ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model) {
+    log.info("objectName={}", bindingResult.getObjectName());
+    log.info("target={}", bindingResult.getTarget());
 
     if (!StringUtils.hasText(item.getItemName())) {
-        bindingResult.addError(
-                new FieldError("item","itemName",item.getItemName(),false,new String[]{"required.item.itemName"},null,null));
+        bindingResult.rejectValue("itemName","required");
     }
     if (item.getPrice() == null || item.getPrice() < 2000 || item.getPrice() > 2000000) {
-        bindingResult.addError(new FieldError("item","price",item.getPrice(),false,new String[]{"range.item.price"},new Object[]{2000,20000},null));
+        bindingResult.rejectValue("price", "range", new Object[]{2000, 2000000}, null);
     }
     if (item.getQuantity() == null || item.getQuantity() >= 9999) {
-        bindingResult.addError(new FieldError("item","quantity",item.getQuantity(),false,new String[]{"max.item.quantity"}, new Object[]{9999}, null));
-    }
+        bindingResult.rejectValue("quantity","max",new Object[]{9999},null);    }
     //특정 필드가 아닌 복합 룰 검증
     if (item.getPrice() != null && item.getQuantity() != null) {
         int resultPrice = item.getPrice() * item.getQuantity();
         if (resultPrice < 20000) {
-            bindingResult.addError(new ObjectError("item", new String[]{"totalPriceMin"},new Object[]{2000,resultPrice},null));
-
+            bindingResult.reject("totalPriceMin", new Object[]{10000, resultPrice}, null);
         }
     }
     //검증에 실패하면 다시 입력 폼으로
