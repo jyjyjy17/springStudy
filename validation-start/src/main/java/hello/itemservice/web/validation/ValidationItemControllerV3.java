62e7543dca8854273b9c5c9e08063749bb73b5c8
avery <pozxc76@pusan.ac.kr>
2024-06-11 16:39:51 +0900

    object error 추가

--- validation-start/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV3.java
+++ validation-start/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV3.java
@@ -44,10 +44,16 @@ public class ValidationItemControllerV3 {
 
 
     @PostMapping("/add")
-    public String addItemV6(@Validated @ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model) {
-        log.info("objectName={}", bindingResult.getObjectName());
-        log.info("target={}", bindingResult.getTarget());
+    public String addItem(@Validated @ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model) {
 
+
+        if (item.getPrice() != null && item.getQuantity() != null) {
+            int resultPrice = item.getPrice() * item.getQuantity();
+            if (resultPrice < 10000) {
+                bindingResult.reject("totalPriceMin", new Object[]{10000,
+                        resultPrice}, null);
+            }
+        }
 
             if (bindingResult.hasErrors()) {
                 log.info("errors={}", bindingResult); // error들 보여줌
