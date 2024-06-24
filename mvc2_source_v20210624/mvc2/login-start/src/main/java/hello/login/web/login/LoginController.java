d710040db9b0dd881290736aaeda6b6deab0d67a
avery <pozxc76@pusan.ac.kr>
2024-06-24 17:47:12 +0900

    [login] session 적용

--- mvc2_source_v20210624/mvc2/login-start/src/main/java/hello/login/web/login/LoginController.java
+++ mvc2_source_v20210624/mvc2/login-start/src/main/java/hello/login/web/login/LoginController.java
@@ -3,6 +3,7 @@ package hello.login.web.login;
 import hello.login.domain.login.LoginService;
 import hello.login.domain.member.Member;
 import hello.login.domain.member.MemberRepository;
+import hello.login.domain.session.SessionManager;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
@@ -11,6 +12,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
 @Slf4j
@@ -19,24 +24,60 @@ import javax.validation.Valid;
 
 public class LoginController {
     private final LoginService loginService;
+    private final SessionManager sessionManager;
 
     @GetMapping("/login")
     public String loginForm(@ModelAttribute("loginForm") LoginForm form) {
         return "login/loginForm";
     }
 
-    @PostMapping("/login")
-    public String login(@Valid @ModelAttribute("loginForm") LoginForm form, BindingResult bindingResult) {
+    /*@PostMapping("/login")
+    public String login(@Valid @ModelAttribute("loginForm") LoginForm form, BindingResult bindingResult, HttpServletResponse response) {
         if (bindingResult.hasErrors()) {
             return "login/loginForm";
 
         }
         Member member = loginService.login(form.getLoginId(), form.getPassword());
         if (member == null) {
-            bindingResult.reject("loginFail","id 또는 비밀번호가 맞지 않습니다");
+            bindingResult.reject("loginFail", "id 또는 비밀번호가 맞지 않습니다");
             return "login/loginForm";
         }
         //login 성공
+        Cookie idCookie = new Cookie("memberId", String.valueOf(member.getId()));
+        response.addCookie(idCookie);
         return "redirect:/";
+    }*/
+    @PostMapping("/login")
+    public String loginV2(@Valid @ModelAttribute("loginForm") LoginForm form, BindingResult bindingResult, HttpServletResponse response) {
+        if (bindingResult.hasErrors()) {
+            return "login/loginForm";
+
+        }
+        Member member = loginService.login(form.getLoginId(), form.getPassword());
+        if (member == null) {
+            bindingResult.reject("loginFail", "id 또는 비밀번호가 맞지 않습니다");
+            return "login/loginForm";
+        }
+        //login 성공
+        sessionManager.createSession(member,response);
+        return "redirect:/";
+    }
+
+    /*@PostMapping("/logout")
+    public String logout(HttpServletResponse response) {
+        expiredCookie(response);
+        return "redirect:/";
+    }*/
+    @PostMapping("/logout")
+    public String logout(HttpServletRequest request, HttpServletResponse response) {
+        sessionManager.expire(request);
+        return "redirect:/";
+    }
+
+    private static void expiredCookie(HttpServletResponse response) {
+        Cookie cookie = new Cookie("memberId", null);
+
+        cookie.setMaxAge(0);
+        response.addCookie(cookie);
     }
 }
