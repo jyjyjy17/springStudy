9114f095d7266ce9b3223607a3455c2196572f2f
avery <pozxc76@pusan.ac.kr>
2024-06-24 23:53:02 +0900

    [login] filter 긑

--- mvc2_source_v20210624/mvc2/login-start/src/main/java/hello/login/web/WebConfig.java
+++ mvc2_source_v20210624/mvc2/login-start/src/main/java/hello/login/web/WebConfig.java
@@ -1,6 +1,7 @@
 package hello.login.web;
 
 import hello.login.web.filter.LogFilter;
+import hello.login.web.filter.LoginCheckFilter;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -18,5 +19,12 @@ public class WebConfig {
         filterrRegistrationBean.addUrlPatterns("/*");
         return filterrRegistrationBean;
     }
-
+    @Bean
+    public FilterRegistrationBean loginCheckFilter() {
+        FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
+        filterRegistrationBean.setFilter(new LoginCheckFilter());
+        filterRegistrationBean.setOrder(2);
+        filterRegistrationBean.addUrlPatterns("/*");
+        return filterRegistrationBean;
+    }
 }
--- mvc2_source_v20210624/mvc2/login-start/src/main/java/hello/login/web/login/LoginController.java
+++ mvc2_source_v20210624/mvc2/login-start/src/main/java/hello/login/web/login/LoginController.java
@@ -11,6 +11,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
@@ -61,7 +62,7 @@ public class LoginController {
         sessionManager.createSession(member,response);
         return "redirect:/";
     }*/
-    @PostMapping("/login")
+    /*@PostMapping("/login")
     public String loginV3(@Valid @ModelAttribute("loginForm") LoginForm form, BindingResult bindingResult, HttpServletRequest request) {
         if (bindingResult.hasErrors()) {
             return "login/loginForm";
@@ -75,6 +76,22 @@ public class LoginController {
         //login 성공 > session 있으면 반환, 없으면 신규 생성
         request.getSession().setAttribute(SessionConst.LOGIN_MEMBER, member);
         return "redirect:/";
+    }*/
+    //REDIRECT URL 쿼리 파라미터로 받는 경우
+    @PostMapping("/login")
+    public String loginV4(@Valid @ModelAttribute("loginForm") LoginForm form, BindingResult bindingResult,@RequestParam(defaultValue = "/") String redirectURL, HttpServletRequest request) {
+        if (bindingResult.hasErrors()) {
+            return "login/loginForm";
+
+        }
+        Member member = loginService.login(form.getLoginId(), form.getPassword());
+        if (member == null) {
+            bindingResult.reject("loginFail", "id 또는 비밀번호가 맞지 않습니다");
+            return "login/loginForm";
+        }
+        //login 성공 > session 있으면 반환, 없으면 신규 생성
+        request.getSession().setAttribute(SessionConst.LOGIN_MEMBER, member);
+        return "redirect:" + redirectURL;
     }
 
     /*@PostMapping("/logout")
