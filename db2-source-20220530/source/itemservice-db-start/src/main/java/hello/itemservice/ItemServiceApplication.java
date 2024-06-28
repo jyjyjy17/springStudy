fb528f4e8b117674f83ed0a74476298444b0a79c
avery <pozxc76@pusan.ac.kr>
2024-06-28 23:16:35 +0900

    [jdbc]끝

--- db2-source-20220530/source/itemservice-db-start/src/main/java/hello/itemservice/ItemServiceApplication.java
+++ db2-source-20220530/source/itemservice-db-start/src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -2,25 +2,25 @@ package hello.itemservice;
 
 import hello.itemservice.config.*;
 import hello.itemservice.repository.ItemRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Profile;
 
-
-@Import(JDBCTemplateV1Config.class)
+@Slf4j
+//@Import(MemoryConfig.class)
+//@Import(JdbcTemplateV1Config.class)
+@Import(JDBCTemplateV2Config.class)
 @SpringBootApplication(scanBasePackages = "hello.itemservice.web")
 public class ItemServiceApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ItemServiceApplication.class, args);
 	}
-//
-//	@Bean
-//	@Profile("local")
-//	public TestDataInit testDataInit(ItemRepository itemRepository) {
-//		return new TestDataInit(itemRepository);
-//	}
-
+	@Bean
+	@Profile("local")
+	public TestDataInit testDataInit(ItemRepository itemRepository) {
+		return new TestDataInit(itemRepository);
+	}
 }
--- db2-source-20220530/source/itemservice-db-start/src/main/java/hello/itemservice/config/JDBCTemplateV2Config.java
+++ db2-source-20220530/source/itemservice-db-start/src/main/java/hello/itemservice/config/JDBCTemplateV2Config.java
@@ -2,6 +2,7 @@ package hello.itemservice.config;
 
 import hello.itemservice.repository.ItemRepository;
 import hello.itemservice.repository.JdbcTemplate.JdbcTemplateItemRepositoryV1;
+import hello.itemservice.repository.JdbcTemplate.JdbcTemplateItemRepositoryV2;
 import hello.itemservice.service.ItemService;
 import hello.itemservice.service.ItemServiceV1;
 import lombok.RequiredArgsConstructor;
@@ -12,17 +13,14 @@ import javax.sql.DataSource;
 
 @Configuration
 @RequiredArgsConstructor
-public class JDBCTemplateV1Config {
-    //spring boot가 자동으로 생성
+public class JDBCTemplateV2Config {
     private final DataSource dataSource;
-
     @Bean
     public ItemService itemService() {
         return new ItemServiceV1(itemRepository());
     }
-
-    private ItemRepository itemRepository() {
-        return new JdbcTemplateItemRepositoryV1(dataSource);
+    @Bean
+    public ItemRepository itemRepository() {
+        return new JdbcTemplateItemRepositoryV2(dataSource);
     }
-
 }
