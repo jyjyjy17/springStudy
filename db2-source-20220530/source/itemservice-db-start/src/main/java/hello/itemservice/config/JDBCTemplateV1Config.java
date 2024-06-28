61a0b07dfcd78520991cc1b4b27657a94057f49c
avery <pozxc76@pusan.ac.kr>
2024-06-28 22:04:42 +0900

    [jdbc] v1 - sql 다 씀

---
+++ db2-source-20220530/source/itemservice-db-start/src/main/java/hello/itemservice/config/JDBCTemplateV1Config.java
@@ -0,0 +1,27 @@
+package hello.itemservice.config;
+
+import hello.itemservice.repository.ItemRepository;
+import hello.itemservice.repository.JdbcTemplate.JdbcTemplateItemRepositoryV1;
+import hello.itemservice.service.ItemService;
+import hello.itemservice.service.ItemServiceV1;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+@RequiredArgsConstructor
+public class JDBCTemplateV1Config {
+    private final DataSource dataSource;
+
+    @Bean
+    public ItemService itemService() {
+        return new ItemServiceV1(itemRepository());
+    }
+
+    private ItemRepository itemRepository() {
+        return new JdbcTemplateItemRepositoryV1(dataSource);
+    }
+
+}
--- jdbc/jdbc/src/main/java/hello/jdbc/DataSourceConfig.java
+++ jdbc/jdbc/src/main/java/hello/jdbc/DataSourceConfig.java
@@ -12,25 +12,25 @@ import javax.sql.DataSource;
 @Configuration
 public class DataSourceConfig {
 
-    @Bean
-    @ConfigurationProperties(prefix = "spring.datasource.h2")
-    public HikariConfig h2Config() {
-        return new HikariConfig();
-    }
-
-    @Bean
-    //@Profile("h2")
-    public DataSource H2DataSource() {
-        return new HikariDataSource(h2Config());
-    }
-    @Bean
-    @ConfigurationProperties(prefix = "spring.datasource.mysql")
-    public HikariConfig MySQLConfig() {
-        return new HikariConfig();
-    }
-
-    @Bean
-    public DataSource MySQLDataSource() {
-        return new HikariDataSource(MySQLConfig());
-    }
+//    @Bean
+//    @ConfigurationProperties(prefix = "spring.datasource.h2")
+//    public HikariConfig h2Config() {
+//        return new HikariConfig();
+//    }
+//
+//    @Bean
+//    //@Profile("h2")
+//    public DataSource H2DataSource() {
+//        return new HikariDataSource(h2Config());
+//    }
+//    @Bean
+//    @ConfigurationProperties(prefix = "spring.datasource.mysql")
+//    public HikariConfig MySQLConfig() {
+//        return new HikariConfig();
+//    }
+//
+//    @Bean
+//    public DataSource MySQLDataSource() {
+//        return new HikariDataSource(MySQLConfig());
+//    }
 }
